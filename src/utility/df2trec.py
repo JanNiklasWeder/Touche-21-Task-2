69e9a456be8a0d2693892d2b8d5836c099add6a8
Jan-Niklas Weder <Jan-Niklas.Weder@live.de>
2021-04-25 14:28:15 +0200

    Added response controls and increased numerical stability.
    UPDATED: The default value of the argument test in combiner is now false.
    
    FIX: load_bert no longer terminates the program if an error occurs in bert. Instead, None is entered as prediction and the corresponding text and topic are logged together with the error.
    
    FIX: query now controls the response code. If a response between 400-600 is returned, an error is raised. Subsequently, the already existing exception handling will take effect.
    
    FIX: df2trec no longer uses the smallest possible number but this number times 100 to avoid numerical errors.

--- src/utility/df2trec.py
+++ src/utility/df2trec.py
@@ -12,7 +12,11 @@ def write(
     team: str = "JackSparrow",
     path: Path = Path.cwd() / "out.trec",
 ) -> None:
+
     ties = df.duplicated(subset=["final"])
+    eps = numpy.finfo(numpy.float64).eps * 100
+    last = len(df.index)
+
     df["order"] = (
         df.sort_values(["final"], ascending=False)
         .groupby("TopicID")
@@ -20,7 +24,8 @@ def write(
     )
 
     while any(ties):
-        df.loc[ties, "final"] = numpy.nextafter(df.loc[ties, "final"], numpy.NINF)
+        first = ties.idxmax()
+        df.loc[first:last, "final"] = df.loc[first:last, "final"] - eps
         ties = df.duplicated(subset=["final"])
 
     df.sort_values(["TopicID", "order"], ascending=True)
@@ -28,17 +33,10 @@ def write(
     with open(path, "w") as filehandle:
         for index, row in tqdm(df.iterrows(), total=df.shape[0], desc="Write to file:"):
             line = (
-                str(row["TopicID"])
-                + " Q0 "
-                + str(row["TrecID"])
-                + " "
-                + str(row["order"])
-                + " "
-                + str(row["final"])
-                + " "
-                + team
-                + "_"
-                + tag
-                + "\n"
+                str(row["TopicID"]) + " Q0 "
+                + str(row["TrecID"]) + " "
+                + str(row["order"]) + " "
+                + str(numpy.format_float_positional(row["final"])) + " "
+                + team + "_" + tag + "\n"
             )
             filehandle.write(line)
