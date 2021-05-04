f9c8cb913d5c0802df86a858806126a0926bedf3
Restyled.io <commits@restyled.io>
2021-05-04 10:30:15 +0000

    Restyled by yapf

--- src/scores/Bert_Docker/load_bert.py
+++ src/scores/Bert_Docker/load_bert.py
@@ -24,9 +24,10 @@ class Bert:
         cache_dir.mkdir(parents=True, exist_ok=True)
         model_args.cache_dir = cache_dir
 
-        self.model = ClassificationModel(
-            "bert", path_to_model, use_cuda=torch.cuda.is_available(), args=model_args
-        )
+        self.model = ClassificationModel("bert",
+                                         path_to_model,
+                                         use_cuda=torch.cuda.is_available(),
+                                         args=model_args)
 
     def predict(self, topic: str, text: str):
         prediction = None
@@ -36,27 +37,26 @@ class Bert:
             logging.info("Raw prediction was : " + str(raw_output))
         except AssertionError as error:
             logging.error(
-                "Coudn't predict Score [Error: {0}]\n".format(error)
-                + "Topic was: "
-                + str(topic)
-                + "\n"
-                "Text  was: " + str(text)
-            )
+                "Coudn't predict Score [Error: {0}]\n".format(error) +
+                "Topic was: " + str(topic) + "\n"
+                "Text  was: " + str(text))
         return prediction
 
     def df_add_score(self, df: pandas.DataFrame):
         combinations = df[["topic", "FullText"]].drop_duplicates()
 
         for index, row in tqdm(
-            combinations.iterrows(),
-            total=combinations.shape[0],
-            desc="Bert score progress:",
+                combinations.iterrows(),
+                total=combinations.shape[0],
+                desc="Bert score progress:",
         ):
             f = io.StringIO()
             with redirect_stderr(f):
                 combinations.loc[index, "Score_Bert"] = self.predict(
-                    row["topic"], row["FullText"]
-                )
+                    row["topic"], row["FullText"])
 
-        result = pandas.merge(df, combinations, on=["topic", "FullText"], how="left")
+        result = pandas.merge(df,
+                              combinations,
+                              on=["topic", "FullText"],
+                              how="left")
         return result
